ca077275a42666a5077fefe35dc6c91e72bb164d
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-07-18 19:50:08 +0300

    done filter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Form } from './Components/Form/Form';
 import { Contacts } from './Components/Contacts/Contacts';
+import { Filter } from './Components/Filter/Filter';
 // import s from './App.module.css';
 
 export class App extends Component {
   state = {
     contacts: [],
+    filter: '',
   };
 
   getSubmitData = data => {
@@ -14,12 +16,26 @@ export class App extends Component {
     });
   };
 
+  changeFilterValue = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  };
+
   render() {
+    const visibleContacts = this.getVisibleContacts();
+
     return (
       <>
         <h1>PhoneBook</h1>
         <Form submitMethod={this.getSubmitData} />
-        <Contacts contacts={this.state.contacts} />
+        <Contacts contacts={visibleContacts} />
+        <Filter value={this.state.filter} onChange={this.changeFilterValue} />
       </>
     );
   }
